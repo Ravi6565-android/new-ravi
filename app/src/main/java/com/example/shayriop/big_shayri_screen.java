4de05ad557b35af1cc0efe28afefdcc6f7ac4c6e
Ravi6565-android <ravichauhanis420@gmail.com>
2023-03-22 11:39:33 +0530

    fineshed shayri

--- app/src/main/java/com/example/shayriop/big_shayri_screen.java
+++ app/src/main/java/com/example/shayriop/big_shayri_screen.java
@@ -12,23 +12,34 @@ import java.util.ArrayList;
 public class big_shayri_screen extends AppCompatActivity implements View.OnClickListener {
 String shayri="kdk";
 Button pre,next;
-int posi=0;
+TextView txtcounter;
+
+int posi=0,size=0;
 ArrayList<String> temp= new ArrayList<>();
 TextView shayri_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_shayri_screen);
+        init();
         shayri_text= findViewById(R.id.big_screen);
         //shayri=getIntent().getStringExtra("shayri");
         temp=getIntent().getStringArrayListExtra("shayri");
         posi=getIntent().getIntExtra("position",0);
         shayri_text.setText(temp.get(posi));
+         size= temp.size();
+        System.out.println("size=="+size);
+        txtcounter.setText(""+(posi+1)+"/"+size);
+
+
+    }
+
+    public void init(){
         pre=findViewById(R.id.pre);
         next=findViewById(R.id.next);
+        txtcounter=findViewById(R.id.txtcounter);
         pre.setOnClickListener(this);
         next.setOnClickListener(this);
-
     }
 
     @Override
@@ -37,12 +48,15 @@ TextView shayri_text;
             if(posi!=0) {
                 posi--;
             }
+
+            txtcounter.setText((posi+1)+"/"+size);
             shayri_text.setText(temp.get(posi));
         }
         if(view.getId()==next.getId()){
             if(posi!=temp.size()-1) {
                 posi++;
             }
+            txtcounter.setText((posi+1)+"/"+size);
             shayri_text.setText(temp.get(posi));
         }
     }
